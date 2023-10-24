c4485197d3bbc9c34c98ba4623d0fe20d4aa52de
Vr3n <vickyspatel@gmail.com>
2023-10-24 22:20:59 +0530

    Add dynamic metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
+    <main className="flex flex-col justify-between p-24">
       <h1 className="text-6xl font-display">Hello, World!</h1>
     </main>
   );
